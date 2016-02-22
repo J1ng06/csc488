2ff4b33b1825dc69437f3670840c10cf3d3c1ed8
Steven Low <lowsteve@users.noreply.github.com>
2016-02-22 11:55:13 -0500

    Added action code for program and constructor for Program.java.

--- src/compiler488/ast/stmt/Program.java
+++ src/compiler488/ast/stmt/Program.java
@@ -1,7 +1,17 @@
 package compiler488.ast.stmt;
 
+import compiler488.ast.ASTList;
+import compiler488.ast.decl.*;
+
 /**
  * Placeholder for the scope that is the entire program
  */
 public class Program extends Scope {
+    private ASTList<Declaration> declarationList;
+    private ASTList<Stmt> statementList;
+    
+    public Program(Scope programScope) {
+        this.declarationList = programScope.getDeclarations();
+        this.statementList = programScope.getStatements();
+    }
 }
